db6386ac3f57cd83a41c13c41eaca8de400e35d6
Amikirami <58829480+Amikirami@users.noreply.github.com>
2024-06-16 19:55:46 +0200

    Update open_camera.py
    open link in new tab

--- open_camera.py
+++ open_camera.py
@@ -1,8 +1,29 @@
 import cv2
 import numpy as np
 import streamlit as st
+from streamlit.components.v1 import html
 
+
+st.title("HIPSTER")
 image = st.camera_input("Show QR code")
+data = ""
+
+def open_page(url):
+    open_script= """
+        <script type="text/javascript">
+            var a = document.createElement("a");
+            a.href = '%s';
+            var evt = document.createEvent("MouseEvents");
+            //the tenth parameter of initMouseEvent sets ctrl key
+            evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0,
+                                        true, false, false, false, 0, null);
+            a.dispatchEvent(evt);
+        </script>
+    """ % (url)
+
+    print(open_script)
+    html(open_script)
+
 
 if image is not None:
     bytes_data = image.getvalue()
@@ -13,4 +34,9 @@ if image is not None:
     data, bbox, straight_qrcode = detector.detectAndDecode(cv2_img)
 
     st.write("Here!")
-    st.write(data)
+st.write(data)
+
+
+
+st.button('PLAY', on_click=open_page, args=(data,))
+
