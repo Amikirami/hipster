0ea3071023e4346f5474bafc5a6bccce08108b98
Amikirami <58829480+Amikirami@users.noreply.github.com>
2024-06-16 18:02:44 +0200

    Update open_camera.py

--- open_camera.py
+++ open_camera.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
 from streamlit_player import st_player
+import cv2
+from pyzbar.pyzbar import decode
 
 class VideoProcessor(VideoProcessorBase):
     def recv(self, frame):
@@ -9,7 +11,10 @@ class VideoProcessor(VideoProcessorBase):
 st.title("Hitster V2.0")
 
 if st.button("Open Camera"):
-    st.camera_input("Take a picture")
+    pic = st.camera_input("Take a picture")
+    #pic = cv2.imread(image_path)
+    decoded_objects = decode(pic)
+    qr_data = [obj.data.decode('utf-8') for obj in decoded_objects]
 
 st_player("https://www.youtube.com/watch?v=BY_XwvKogC8&list=RDBY_XwvKogC8&start_radio=1")
 
